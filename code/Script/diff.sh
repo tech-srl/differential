07146a4c81d1b5389701f0023d0ad70d688f3821
nimrodpar <nimrodpar@gmail.com>
2013-08-06 08:52:15 -0700

    - added the DataflowSolver.h file - updated the install instructions - small change to the directory diff script

--- code/Script/diff.sh
+++ code/Script/diff.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-echo "Usage: $(basename $0) <compare-dir>"
-echo "Diffs all the c files in this dir with the files in <compare-dir> and puts the different files,"
-echo "alongside the files from the <compare-dir> under tmp (with patched.* prefix)"
+function usage {
+	echo "Usage: $(basename $0) <compare-dir>"
+	echo "Diffs all the c files in this dir with the files in <compare-dir> and puts the different files,"
+	echo "alongside the files from the <compare-dir> under tmp (with patched.* prefix)"
+}
+
 if [[ $# < 1 ]]
     then
+	usage
         echo "Not enough args, exiting"
         exit
 fi
-echo "Press 'Enter' to continue or terminate with 'Ctrl+c'"
-read -n1 kbd
+
 mkdir tmp 2>/dev/null
 dir=$(echo $1 | sed 's/\/$//')
-for file in $( ls *.c ); do
+for file in $( ls *.i ); do
 	echo diff $file $dir/$file
 	if [[ $(diff $file $dir/$file) != "" ]]
-    then
-		file=$(echo $file | sed 's/c$/i/')
+	    then
+#		file=$(echo $file | sed 's/c$/i/')
 		echo "cp $file tmp/"
-        cp $file tmp/
-		echo "cp $dir/$file tmp/"
-		cp ../../coreutils-6.11/src/$file tmp/patched.$file
+        	cp $file tmp/$file
+		echo "cp $dir/$file tmp/patched.$file"
+		cp $dir/$file tmp/patched.$file
 	fi
 done
 
