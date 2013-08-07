9cf6f743bb3be10a0bada5b703eef53346d4555c
nimrodpar <nimrodpar@gmail.com>
2013-08-07 18:34:18 +0300

    removed teh doc dir

--- doc/phd-thesis/figures/makeall.bat
+++
@@ -1,4 +0,0 @@
-call pdf2eps minimal
-call pdf2eps geom-af1
-call pdf2eps geom-af2
-call pdf2eps geom-values
--- doc/popl12-paper/figures/dotall.bat
+++
@@ -1,4 +0,0 @@
-dot -Tpdf -osign.pdf < sign.dt
-dot -Tpdf -osign-tag.pdf < sign-tag.dt
-dot -Tpdf -osign-product.pdf < sign-product.dt
-dot -Tpdf -osign-correlated.pdf < sign-correlated.dt
