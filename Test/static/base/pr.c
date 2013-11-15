82e631135d6415af124772b04c0214805baae37b
nimrodpar <user@ubuntu>
2013-11-15 03:35:07 +0200

    pr adjustment

--- Test/static/base/pr.c
+++ Test/static/base/pr.c
@@ -2,7 +2,7 @@ typedef int bool;
 
 #define false (0)
 #define true  (1)
-#define TAB_WIDTH(c_, h_) ((c_) - ((h_) % (c_)))
+#define TAB_WIDTH(c_, h_) ((c_) - (h_))
 
 extern bool isprint(char);
 extern int sprintf(char*,...);
@@ -10,97 +10,102 @@ extern int sprintf(char*,...);
 static int
 char_to_clump (char c)
 {
-  static char *clump_buff;
-  static char input_tab_char = '\t';
-  static int chars_per_input_tab = 8;
-  static int input_position;
-  static bool untabify_input = false;
-  static bool use_esc_sequence = false;
-  static bool use_cntrl_prefix = false;
-  unsigned char uc = c;
-  char *s = clump_buff;
-  int i;
-  char esc_buff[4];
-  int width;
-  int chars;
-  int chars_per_c = 8;
+	static char *clump_buff;
+	static char input_tab_char = '\t';
+	static int chars_per_input_tab = 8;
+	static int input_position;
+	static bool untabify_input = false;
+	static bool use_esc_sequence = false;
+	static bool use_cntrl_prefix = false;
+	unsigned char uc = c;
+	char *s = clump_buff;
+	int i;
+	char esc_buff[4];
+	int width;
+	int chars;
+	int chars_per_c = 8;
 
-  if (c == input_tab_char)
-    chars_per_c = chars_per_input_tab;
+	if (c == input_tab_char)
+		chars_per_c = chars_per_input_tab;
 
-  if (c == input_tab_char || c == '\t')
-    {
-      width = TAB_WIDTH (chars_per_c, input_position);
-
-      if (untabify_input)
+	if (c == input_tab_char || c == '\t')
 	{
+		width = TAB_WIDTH (chars_per_c, input_position);
 
-	  for (i = width; i; --i)
-	    *s++ = ' ';
+		if (untabify_input)
+		{
+			i = width;
+			loop1:	  if (i > 0) {
+				*s++ = ' ';
+				--i;
+				goto loop1;
+			}
 
-	  chars = width;
-	}
-      else
-	{
-	  *s = c;
-	  chars = 1;
-	}
-
-    }
-    else if (! isprint (uc))
-    {
-      if (use_esc_sequence)
-	{
-	  width = 4;
-	  chars = 4;
-	  *s++ = '\\';
-	  sprintf (esc_buff, "%03o", uc);
-
-	  for (i = 0; i <= 2; ++i)
-	    *s++ = esc_buff[i];
+			chars = width;
+		}
+		else
+		{
+			*s = c;
+			chars = 1;
+		}
 
 	}
-      else if (use_cntrl_prefix)
+	else if (! isprint (uc))
 	{
-	  if (uc < 0200)
-	    {
-	      width = 2;
-	      chars = 2;
-	      *s++ = '^';
-	      *s++ = c ^ 0100;
-	    }
-	  else
-	    {
-	      width = 4;
-	      chars = 4;
-	      *s++ = '\\';
-	      sprintf (esc_buff, "%03o", uc);
+		if (use_esc_sequence)
+		{
+			width = 4;
+			chars = 4;
+			*s++ = '\\';
+			sprintf (esc_buff, "%03o", uc);
 
-	      for (i = 0; i <= 2; ++i)
-		*s++ = esc_buff[i];
+			*s++ = esc_buff[0];
+			*s++ = esc_buff[1];
+			*s++ = esc_buff[2];
 
-	    }
+		}
+		else if (use_cntrl_prefix)
+		{
+			if (uc < 0200)
+			{
+				width = 2;
+				chars = 2;
+				*s++ = '^';
+				*s++ = c ^ 0100;
+			}
+			else
+			{
+				width = 4;
+				chars = 4;
+				*s++ = '\\';
+				sprintf (esc_buff, "%03o", uc);
+
+				*s++ = esc_buff[0];
+				*s++ = esc_buff[1];
+				*s++ = esc_buff[2];
+
+			}
+		}
+		else if (c == '\b')
+		{
+			width = -1;
+			chars = 1;
+			*s = c;
+		}
+		else
+		{
+			width = 0;
+			chars = 1;
+			*s = c;
+		}
 	}
-      else if (c == '\b')
+	else
 	{
-	  width = -1;
-	  chars = 1;
-	  *s = c;
+		width = 1;
+		chars = 1;
+		*s = c;
 	}
-      else
-	{
-	  width = 0;
-	  chars = 1;
-	  *s = c;
-	}
-    }
-  else
-    {
-      width = 1;
-      chars = 1;
-      *s = c;
-    }
 
-  input_position += width;
-  return chars;
+	input_position += width;
+	return chars;
 }
