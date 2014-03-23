b101197ac17e5a108177c6a36bf817c4916b31c7
nimrodpar <nimrodpar@gmail.com>
2014-03-23 09:33:14 -0700

    fixed remove test

--- Test/coreutils/remove/cache_fstatat.v610.c
+++ Test/coreutils/remove/cache_fstatat.v610.c
@@ -5,16 +5,16 @@
 static int
 cache_fstatat (int fd, char const *file, int st_size, int st_ino, int flag)
 {
-  static int errno;
-  static int fstatat;
-  if (st_size == -1 && fstatat != 0)
-    {
-      st_size = -2;
-      st_ino = errno;
-    }
-  if (0 <= st_size)
-    return 0;
-  errno = (int) st_ino;
-  return -1;
+	static int errno = 0;
+	static int fstatat = 0;
+	if (st_size == -1 && fstatat != 0)
+	{
+		st_size = -2;
+		st_ino = errno;
+	}
+	if (0 <= st_size)
+		return 0;
+	errno = (int) st_ino;
+	return -1;
 }
 
--- Test/coreutils/remove/cache_fstatat.v69.c
+++ Test/coreutils/remove/cache_fstatat.v69.c
@@ -5,13 +5,13 @@
 static int
 cache_fstatat (int fd, char const *file, int st_size, int st_ino, int flag)
 {
-  static int errno;
-  static int fstatat;
-  if (st_size == -1 && fstatat != 0)
-    st_size = -1 - errno;
-  if (0 <= st_size)
-    return 0;
-  errno = -1 - st_size;
-  return -1;
+	static int errno = 0;
+	static int fstatat = 0;
+	if (st_size == -1 && fstatat != 0)
+		st_size = -1 - errno;
+	if (0 <= st_size)
+		return 0;
+	errno = -1 - st_size;
+	return -1;
 }
 
