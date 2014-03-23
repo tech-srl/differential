7cd963e5d4341b775ab226625548dae9cc1adc2f
nimrodpar <nimrodpar@gmail.com>
2014-03-23 09:57:51 -0700

    copy test done

--- Test/coreutils/copy/set_owner.v0.c
+++ Test/coreutils/copy/set_owner.v0.c
@@ -1,10 +1,9 @@
-#define error(...)
-#define true 1
-#define false 0
-
 typedef int uid_t;
 typedef int gid_t;
-typedef int bool;
+
+extern int HAVE_FCHOWN(...);
+extern int error(...);
+extern int quote(...);
 
 /* Set the owner and owning group of DEST_DESC to the st_uid and
    st_gid fields of SRC_SB.  If DEST_DESC is undefined (-1), set
@@ -13,40 +12,37 @@ typedef int bool;
    Return true if the syscall succeeds, or if it's ok not to
    preserve ownership.  */
 
-static bool
+static int
 set_owner (
-char const *dst_name, 
-int dest_desc, 
-uid_t uid, 
-gid_t gid)
+		char const *dst_name,
+		int dest_desc,
+		uid_t uid,
+		gid_t gid,
+		int fchown_dest_desc_uid_gid,
+		int chown_dst_name_uid_gid,
+		int chown_failure_ok_x,
+		int x_require_preserve,
+		int errno)
 {
-	static int HAVE_FCHOWN;
-	static int fchown_dest_desc_uid_gid;
-	static int chown_dst_name_uid_gid;
-	static int chown_failure_ok_x;
-	static int x_require_preserve;
-	int r = true;
-	if (HAVE_FCHOWN && dest_desc != -1)
+	int r = 1;
+	if (HAVE_FCHOWN() && dest_desc != -1)
 	{
 		if (fchown_dest_desc_uid_gid == 0) {
-			r = true;
 			return r;
 		}
 	}
 	else
 	{
 		if (chown_dst_name_uid_gid == 0) {
-			r = true;
 			return r;
 		}
 	}
 
 	if (! chown_failure_ok_x)
 	{
-		error (0, errno, _("failed to preserve ownership for %s"),
+		error (0, errno, "failed to preserve ownership for %s",
 				quote (dst_name));
 		if (x_require_preserve) {
-			r = true;
 			return r;
 		}
 	}
--- Test/coreutils/copy/set_owner.v1.c
+++ Test/coreutils/copy/set_owner.v1.c
@@ -1,10 +1,9 @@
-#define error(...)
-#define true 1
-#define false 0
-
 typedef int uid_t;
 typedef int gid_t;
-typedef int bool;
+
+extern int HAVE_FCHOWN(...);
+extern int error(...);
+extern int quote(...);
 
 /* Set the owner and owning group of DEST_DESC to the st_uid and
    st_gid fields of SRC_SB.  If DEST_DESC is undefined (-1), set
@@ -14,32 +13,34 @@ typedef int bool;
    not to preserve ownership, -1 otherwise.  */
 
 static int
-set_owner (char const *dst_name, int dest_desc, uid_t uid, gid_t gid)
+set_owner (
+		char const *dst_name,
+		int dest_desc,
+		uid_t uid,
+		gid_t gid,
+		int fchown_dest_desc_uid_gid,
+		int chown_dst_name_uid_gid,
+		int chown_failure_ok_x,
+		int x_require_preserve,
+		int errno)
 {
-	static int HAVE_FCHOWN;
-	static int fchown_dest_desc_uid_gid;
-	static int chown_dst_name_uid_gid;
-	static int chown_failure_ok_x;
-	static int x_require_preserve;
-	int r = 0;
-	if (HAVE_FCHOWN && dest_desc != -1)
+	int r = 1;
+	if (HAVE_FCHOWN() && dest_desc != -1)
 	{
 		if (fchown_dest_desc_uid_gid == 0) {
-			r = 1;
 			return r;
 		}
 	}
 	else
 	{
 		if (chown_dst_name_uid_gid == 0){
-			r = 1;
 			return r;
 		}
 	}
 
 	if (! chown_failure_ok_x)
 	{
-		error (0, errno, _("failed to preserve ownership for %s"),
+		error (0, errno, "failed to preserve ownership for %s",
 				quote (dst_name));
 		if (x_require_preserve){
 			r = -1;
